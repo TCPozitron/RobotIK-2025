ccaeaea2765e20f5577edbe4e41cd2d2509ae03a
Lovro Šverko <lovro.sverko@gmail.com>
2025-10-22 13:47:55 +0200

    Update gyro-imu.ino

--- primjeri/gyro-imu.ino
+++ primjeri/gyro-imu.ino
@@ -1,172 +1,170 @@
-/******************************************************************************
- *
- * PROJEKT: STABILNA ORIJENTACIJA ROBOTA (HEADNING)
- *
- * @file        LSM9DS1_Orijentacija_Robota.ino
- * @brief       Edukativni kod za Arduino Mega pločicu.
- *
- * @description
- * Ovaj program koristi SVA TRI senzora unutar LSM9DS1 čipa (akcelerometar,
- * giroskop i magnetometar) kako bi izračunao stabilan kut orijentacije
- * oko Z-osi (yaw / heading). Koristi algoritam senzorske fuzije kako bi
- * eliminirao drift giroskopa. Rezultat je pouzdan "kompas" za robota.
- *
- * SVE UPUTE ZA RAD NALAZE SE U OVOM KODU KAO KOMENTARI.
- *
- ******************************************************************************
- *
- * UPUTE ZA UČENIKE - KAKO POKRENUTI PROJEKT
- *
- ******************************************************************************
- *
- * KORAK 1: POTREBNE KOMPONENTE
- * 1. Arduino Mega 2560 pločica
- * 2. LSM9DS1 9-osni senzor (na modulu/pločici)
- * 3. Spojne žice (4 komada)
- *
- ******************************************************************************
- *
- * KORAK 2: SPAJANJE ŽICA (Isto kao i prije)
- *
- * Senzor se spaja na I2C pinove Arduino Mega ploče.
- *
- * +----------------+--------------------+
- * | PIN NA SENZORU | ARDUINO MEGA PIN   |
- * +----------------+--------------------+
- * | VCC (ili 5V)   | 5V                 |
- * | GND            | GND                |
- * | SDA            | 20                 |
- * | SCL            | 21                 |
- * +----------------+--------------------+
- *
- ******************************************************************************
- *
- * KORAK 3: INSTALACIJA SVIH POTREBNIH BIBLIOTEKA
- *
- * Za ovaj napredni pristup, trebamo nekoliko biblioteka koje rade zajedno.
- * Sve se instaliraju na isti način:
- * Sketch > Include Library > Manage Libraries...
- *
- * Potrebno je instalirati SVE ČETIRI biblioteke:
- * 1. Adafruit LSM9DS1 Library (od Adafruit)
- * 2. Adafruit Sensor Fusion (od Adafruit)
- * 3. Adafruit Unified Sensor (od Adafruit) -> ovo je ovisnost (dependency)
- * 4. Adafruit BusIO (od Adafruit) -> ovo je ovisnost (dependency)
- *
- ******************************************************************************
- *
- * KORAK 4: KALIBRACIJA MAGNETOMETRA - NAJVAŽNIJI KORAK!
- *
- * Magnetometar (kompas) je osjetljiv na metal i magnetska polja u okolini.
- * Da bi davao točne rezultate, MORAMO ga kalibrirati.
- *
- * KAKO KALIBRIRATI:
- * 1. U Arduino IDE, idite na: File > Examples > Adafruit LSM9DS1 Library > calib
- * 2. Učitajte taj "calib" primjer na vaš Arduino.
- * 3. Otvorite Serial Plotter (Tools > Serial Plotter).
- * 4. Polako okrećite senzor u svim smjerovima, iscrtavajući "osmice" u zraku.
- * Činite to oko minutu dok se vrijednosti na grafu ne stabiliziraju.
- * 5. Prebacite se na Serial Monitor. Tamo će biti ispisane minimalne i
- * maksimalne vrijednosti za X, Y i Z os magnetometra.
- * 6. ZAPIŠITE te vrijednosti. Trebat će vam za korak 5.
- * Izgledat će otprilike ovako:
- * Mag Min: -60.89, -79.94, -66.00
- * Mag Max: 49.92, 34.41, 41.54
- *
- ******************************************************************************
- *
- * KORAK 5: UČITAVANJE GLAVNOG KODA (ovog koda)
- *
- * 1. U kodu ispod, pronađite sekciju "--- UNESITE VRIJEDNOSTI KALIBRACIJE OVDJE ---".
- * 2. Unesite minimalne i maksimalne vrijednosti koje ste zapisali u koraku 4.
- * 3. Učitajte ovaj kod na vašu Arduino Mega pločicu.
- * 4. Otvorite Serial Monitor (brzina 9600).
- * 5. Vidjet ćete stabilan kut od 0 do 360 stupnjeva koji se ne mijenja
- * kada senzor miruje!
- *
- ******************************************************************************/
+// 1. UKLJUČI NOVU BIBLIOTEKU
+#include <SparkFunLSM9DS1.h>
+#include "Wire.h"
 
+/* ... Dijagram spajanja ... (isti je) */
 
-// ------- POČETAK KODA -------
+// 2. KREIRAJ OBJEKT IZ NOVE KLASE
+LSM9DS1 imu;
 
-// UKLJUČIVANJE SVIH POTREBNIH BIBLIOTEKA
-#include <Wire.h>
-#include <Adafruit_LSM9DS1.h>
-#include <Adafruit_Sensor_Fusion.h>
-#include <Adafruit_Sensor.h>
+// 3. ZALIJEPITE SVOJE VRIJEDNOSTI OVDJE
+// (Ovo je linija koju ste kopirali iz Koraka 1)
+float magBias[3] = { 749.500000f, 457.000000f, 373.000000f };
+//float magBias[3] = { 305.000000f, 185.500000f, 1095.000000f };
 
-// --- UNESITE VRIJEDNOSTI KALIBRACIJE OVDJE ---
-// Zamijenite ove primjere s vrijednostima koje ste dobili u KORAKU 4.
-// Format je: {X_MIN, Y_MIN, Z_MIN, X_MAX, Y_MAX, Z_MAX}
-float mag_min[3] = {-60.89, -79.94, -66.00};
-float mag_max[3] = {49.92,  34.41,  41.54};
+// Postavke ispisa...
+#define PRINT_CALCULATED
+#define PRINT_SPEED 250 
+static unsigned long lastPrint = 0; 
 
-// Stvaranje objekta za senzor.
-Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+// 4. ISPRAVAK DEKLINACIJE
+#define DECLINATION 4.8 // Deklinacija za Pazin, Istra
 
-// Pomoćni objekti za pristup pojedinačnim senzorima unutar čipa.
-Adafruit_Sensor *accelerometer, *magnetometer, *gyroscope;
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin();
 
-// Filter za senzorsku fuziju. On će raditi svu tešku matematiku.
-// Koristimo Mahony filter, koji je brz i dobar za Arduino.
-Adafruit_Sensor_Fusion filter;
-
-// SETUP FUNKCIJA - Izvršava se samo jednom.
-void setup() {
-    Serial.begin(9600);
-    while (!Serial);
-
-    // Inicijalizacija senzora
-    if (!lsm.begin()) {
-        Serial.println("Greska pri inicijalizaciji LSM9DS1 senzora!");
-        while (1);
+    // 5. KORISTI NOVI .begin()
+    if (imu.begin() == false) 
+    {
+        Serial.println("Failed to communicate with LSM9DS1.");
+        Serial.println("Double-check the wiring!");
+        while (1) { delay(100); }
     }
-    Serial.println("LSM9DS1 senzor pronadjen!");
-
-    // "Povezivanje" naših pomoćnih objekata sa stvarnim senzorima.
-    accelerometer = lsm.getAccel();
-    magnetometer  = lsm.getMag();
-    gyroscope     = lsm.getGyro();
     
-    // Pokretanje filtera za fuziju. Argument je brzina ažuriranja u Hz.
-    filter.begin(100); // 100 Hz je dobra brzina.
+    // 6. MAKNULI SMO 'setMagBiases()' JER NE POSTOJI
+    
+    Serial.println("Senzor inicijaliran. Koristim rucnu kalibraciju."); 
 }
 
-// LOOP FUNKCIJA - Ponavlja se u krug.
-void loop() {
-    // Varijable za spremanje podataka sa senzora.
-    sensors_event_t accel_event, mag_event, gyro_event;
 
-    // Očitavanje podataka sa sva tri senzora.
-    accelerometer->getEvent(&accel_event);
-    magnetometer->getEvent(&mag_event);
-    gyroscope->getEvent(&gyro_event);
-
-    // --- Primjena kalibracije na podatke magnetometra ---
-    // Ovo je ključan korak za točan smjer!
-    float mag_x = mag_event.magnetic.x - (mag_min[0] + mag_max[0]) / 2;
-    float mag_y = mag_event.magnetic.y - (mag_min[1] + mag_max[1]) / 2;
-    float mag_z = mag_event.magnetic.z - (mag_min[2] + mag_max[2]) / 2;
+void loop()
+{
+    // Ažuriraj senzore
+    if (imu.gyroAvailable()) { imu.readGyro(); }
+    if (imu.accelAvailable()) { imu.readAccel(); }
     
-    // "Nahranimo" filter svježim podacima sa senzora.
-    // Jedinice moraju biti rad/s za giroskop i m/s^2 za akcelerometar,
-    // što su standardne jedinice koje Adafruit biblioteka daje.
-    filter.update(gyro_event.gyro.x, gyro_event.gyro.y, gyro_event.gyro.z,
-                  accel_event.acceleration.x, accel_event.acceleration.y, accel_event.acceleration.z,
-                  mag_x, mag_y, mag_z);
+    // --- OVDJE JE KLJUČNA PROMJENA ---
+    if (imu.magAvailable()) 
+    { 
+        // 1. Očitaj sirove (RAW) podatke
+        imu.readMag(); 
+        
+        // 2. Ručno oduzmi pogrešku (bias) od sirovih vrijednosti
+        // Pretvaramo float bias natrag u int16_t za oduzimanje
+        imu.mx = imu.mx - (int16_t)magBias[0];
+        imu.my = imu.my - (int16_t)magBias[1];
+        imu.mz = imu.mz - (int16_t)magBias[2];
+    }
+    // --- KRAJ PROMJENE ---
 
-    // Dohvaćanje rezultata iz filtera
-    float roll, pitch, heading;
-    roll    = filter.getRoll();
-    pitch   = filter.getPitch();
-    heading = filter.getYaw(); // Yaw je ono što nas zanima - smjer!
 
-    // Ispis rezultata
-    Serial.print("Orijentacija (Heading/Smjer): ");
-    Serial.print(heading);
-    Serial.println(" stupnjeva");
+    // Ispis (potpuno isto)
+    if ((lastPrint + PRINT_SPEED) < millis())
+    {
+        // Sada će printMag() i printAttitude() automatski koristiti
+        // ISPRAVLJENE 'imu.mx', 'imu.my' i 'imu.mz' vrijednosti.
+        
+        printGyro(); 
+        printAccel();
+        printMag();
+        printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+        Serial.println();
+        lastPrint = millis(); 
+    }
+}
 
-    // Pauza kako bi se uskladili s brzinom filtera (100 Hz -> 10ms)
-    // i da ne pretrpamo Serial Monitor.
-    delay(100);
+//
+// SVE FUNKCIJE ISPOD OSTAJU POTPUNO ISTE
+//
+
+void printGyro()
+{
+    Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+    Serial.print(imu.calcGyro(imu.gx), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcGyro(imu.gy), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcGyro(imu.gz), 2);
+    Serial.println(" deg/s");
+#elif defined PRINT_RAW
+    Serial.print(imu.gx);
+    Serial.print(", ");
+    Serial.print(imu.gy);
+    Serial.print(", ");
+    Serial.println(imu.gz);
+#endif
+}
+
+void printAccel()
+{
+    Serial.print("A: ");
+#ifdef PRINT_CALCULATED
+    Serial.print(imu.calcAccel(imu.ax), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcAccel(imu.ay), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcAccel(imu.az), 2);
+    Serial.println(" g");
+#elif defined PRINT_RAW
+    Serial.print(imu.ax);
+    Serial.print(", ");
+    Serial.print(imu.ay);
+    Serial.print(", ");
+    Serial.println(imu.az);
+#endif
+}
+
+void printMag()
+{
+    Serial.print("M: ");
+#ifdef PRINT_CALCULATED
+    // Ova funkcija sada koristi ispravljene (kalibrirane)
+    // vrijednosti imu.mx, imu.my, imu.mz
+    Serial.print(imu.calcMag(imu.mx), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcMag(imu.my), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcMag(imu.mz), 2);
+    Serial.println(" gauss");
+#elif defined PRINT_RAW
+    Serial.print(imu.mx);
+    Serial.print(", ");
+    Serial.print(imu.my);
+    Serial.print(", ");
+    Serial.println(imu.mz);
+#endif
+}
+
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+    float roll = atan2(ay, az);
+    float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+
+    float heading;
+    if (my == 0)
+        heading = (mx < 0) ? PI : 0;
+    else
+        // Ova funkcija sada također koristi kalibrirane vrijednosti
+        heading = atan2(mx, my);
+
+    heading -= DECLINATION * PI / 180;
+
+    if (heading > PI)
+        heading -= (2 * PI);
+    else if (heading < -PI)
+        heading += (2 * PI);
+
+    heading *= 180.0 / PI;
+    pitch *= 180.0 / PI;
+    roll *= 180.0 / PI;
+
+    Serial.print("Pitch, Roll: ");
+    Serial.print(pitch, 2);
+    Serial.print(", ");
+    Serial.println(roll, 2);
+    Serial.print("Heading: ");
+    Serial.println(heading, 2);
 }
